59a200206181a53529797884f5c0ffd6d52b899b
Gaurav <gauravgurung065@gmail.com>
2021-08-17 15:17:11 +0545

    create client-side template using react ts

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[Controller]")]
+    [Route("api/[Controller]")]
     public class BaseApiController: ControllerBase
     {
         
