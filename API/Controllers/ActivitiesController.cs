8e5bba16ff0f45069b031119e81413be42a5757c
Gaurav <gauravgurung065@gmail.com>
2021-08-20 16:34:20 +0545

    CreateHandler created

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,27 +5,25 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using MediatR;
 using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            this._mediator = mediator;
-        }
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id) {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
 
          }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity (Activity activity) {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        } 
     }
 }  
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,7 @@
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -6,6 +9,8 @@ namespace API.Controllers
     [Route("api/[Controller]")]
     public class BaseApiController: ControllerBase
     {
-        
+        private  IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
